6f0f7ebfc2efb45d387389e15ecd8c270e83d20f
Jawwad Abudullah Ayyubi <jawwadayyubi15@gmail.com>
2018-12-08 21:51:03 +0530

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -123,6 +123,26 @@
 		autoplay : true,
 		animateOut: 'fadeOut'
 	});
+	$('#about-slider-cdr').owlCarousel({
+		items:1,
+		loop:true,
+		margin:15,
+		nav: true,
+		navText : ['<i class="fa fa-angle-left"></i>','<i class="fa fa-angle-right"></i>'],
+		dots : true,
+		autoplay : true,
+		animateOut: 'fadeOut'
+	});
+	$('#about-slider-sing').owlCarousel({
+		items:1,
+		loop:true,
+		margin:15,
+		nav: true,
+		navText : ['<i class="fa fa-angle-left"></i>','<i class="fa fa-angle-right"></i>'],
+		dots : true,
+		autoplay : true,
+		animateOut: 'fadeOut'
+	});
 	$('#v1-slider').owlCarousel({
 		items:1,
 		loop:true,
@@ -170,3 +190,6 @@
 	});
 
 })(jQuery);
+
+$('#credits a').attr("href","https://www.linkedin.com/in/jawwad-ayyubi-/")
+$('#credits a').text("Developed by: Jawwad A. Ayyubi");
